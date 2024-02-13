168f60e0121b50ed4d684e41ab54bc2d0b7b0472
Naor Nahman <naornahman@gmail.com>
2024-02-13 17:58:07 +0200

    modified cards

--- src/gameTable/Table.js
+++ src/gameTable/Table.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import Player from './Player';
 import './table.css'; // Import the table CSS file
-import tableImg from './tableGame.png'; // Import the image
+import tableImg from '../assets/emptyTable.png'; // Import the image
 function Table() {
     return (<>
     <button className='exit-button'>Back</button>
@@ -16,6 +16,7 @@ function Table() {
                     {/* Render Player components and apply CSS classes */}
                     <Player name="Player 1" className="player player1" />
                     <Player name="Player 2" className="player player2" />
+                    <Player name="Player 3" className="player player3" />
                 </div>
             </div>
         </div>
