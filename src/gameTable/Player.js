0cd9c3ef91cf04c80344a556b2b3f8fd3a6e7418
Naor Nahman <naornahman@gmail.com>
2024-02-20 17:53:10 +0200

    changes in draw card
    Signed-off-by: Naor Nahman <naornahman@gmail.com>

--- src/gameTable/Player.js
+++ src/gameTable/Player.js
@@ -1,20 +1,26 @@
-// Player.js
-
 import React, { useState } from 'react';
-import RandomCard from './RandomCard';
+import RandomTwoCards from './RandomCard';
 import './table.css';
 import cards from '../assets/cards.png';
 
 const Player = (props) => {
     // State to track whether to show cards or not
     const [showCards, setShowCards] = useState(true);
+    // State to store the generated card
+    const [generatedCards, setGeneratedCard] = useState(null);  
+
+    // Initial generation of random cards, if not already generated
+    if (!generatedCards) {
+        const newCards = RandomTwoCards(); // Assuming RandomCard returns a card
+        setGeneratedCard(newCards);
+    }
 
     return (
         <div className={props.className}>
             <span className="player-name">{props.name}</span>
             <div className="player-cards">
                 {showCards ? (
-                    <RandomCard />
+                    generatedCards // Display the stored cards
                 ) : (
                     <img className="hidden-cards" src={cards} alt="Player Cards" />
                 )}
