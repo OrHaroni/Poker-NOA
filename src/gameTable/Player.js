3ef7e6aafbd0697b7f7e1d3ee1f39463431c0a2c
OrHaroni <or.haroni@gmail.com>
2024-06-17 08:56:45 +0300

    added profile pics for users and ai bots
    Signed-off-by: OrHaroni <or.haroni@gmail.com>

--- src/gameTable/Player.js
+++ src/gameTable/Player.js
@@ -3,11 +3,16 @@ import './table.css';
 import cards from '../assets/cards.png';
 import Timer from '../Animations/AnimatedTimer/Timer';
 import logo from '../assets/logopng.png'
+import genericPic from '../assets/generic_profile_pic.jpg'
+import aiPic from '../assets/gemini_profile_pic.jpeg'
 
 const Player = (props) => {
+
+    const profilePic = props.ai ? aiPic : genericPic
     
     return (
         <div className={props.className}>
+            <img className='profile-pic' src={profilePic}/>
             {props.timer ? <Timer time={20}/> : null}
             <span className="player-name">{props.name}</span><br/>
             <img src={logo} alt="Logo" className="money-logo" />{props.money}$
